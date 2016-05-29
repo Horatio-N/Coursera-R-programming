062997d1aeb7fe99b75ef038bb72c8008e7ab13a
Horatio-N <nguyenthanhhung1105@gmail.com>
2016-05-29 10:50:24 +0000

    Add files via upload

---
+++ corr.R
@@ -0,0 +1,20 @@
+corr <- function(directory, threshold = 0) {
+        ## Create container vector
+        cor.vec <- NULL
+        ## List out all files in specdata folder
+        csv.files <- list.files(paste('./', directory, '/', sep = ''))
+        ## Generate path to files
+        path.gen <- paste(getwd(), '/', directory, '/', csv.files, sep = '')
+        ## Calculate correlation
+        for (i in 1:length(csv.files)){
+                ## Read file
+                d.cor <- read.csv(path.gen[i])
+                ## Checking criteria
+                if(sum(complete.cases(d.cor)) <= threshold)next
+                        ## Remove NA values
+                        d.clean <- d.cor[complete.cases(d.cor),]
+                        ## Fletching data
+                        cor.vec <- c(cor.vec, cor(d.clean$sulfate, d.clean$nitrate))        
+        }
+        cor.vec
+}
