fae57e7442cda8a129ad67851aeb786b3aa1b5a0
Horatio-N <nguyenthanhhung1105@gmail.com>
2016-05-29 17:53:17 +0700

    Update corr.R

--- corr.R
+++ corr.R
@@ -5,12 +5,12 @@ corr <- function(directory, threshold = 0) {
         cor.vec <- NULL
         ## List out all files in specdata folder
         csv.files <- list.files(paste('./', directory, '/', sep = ''))
-        ## Generate path to files
-        path.gen <- paste(getwd(), '/', directory, '/', csv.files, sep = '')
+        ## Create path to csv files
+        path <- paste(getwd(), '/', directory, '/', csv.files, sep = '')
         ## Calculate correlation
         for (i in 1:length(csv.files)){
                 ## Read file
-                d.cor <- read.csv(path.gen[i])
+                d.cor <- read.csv(path[i])
                 ## Checking criteria
                 if(sum(complete.cases(d.cor)) <= threshold)next
                         ## Remove NA values
